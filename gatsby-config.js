3559eab690369b276510f01eebd15f0435742c8d
teitei-tk <teitei.tk@gmail.com>
2019-01-01 18:47:56 +0900

    render note feed component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,27 @@
 module.exports = {
   siteMetadata: {
     name: "Hello Typescript Gatsby World!",
-    tagline: "Gatsby + SASS + Typescript = 💪"
+    tagline: "Gatsby + SASS + Typescript = 💪",
+    note: {
+      url: "https://note.mu/teitei_tk",
+      posts: [
+        {
+          title:
+            "Ruby on RailsのJSON API Responseについて考える。あるいはjbuilderについての個人的違和感とその解",
+          link: "https://note.mu/teitei_tk/n/nc6309350648a"
+        },
+        {
+          title:
+            "第三少女飛行隊から憶測するフレンズ問題について。あるいは宇宙よりも遠い場所という作品から得た希望",
+          link: "https://note.mu/teitei_tk/n/n4d2f0d0fb332"
+        },
+        {
+          title:
+            "VSCodeの設定ファイルやキーバインド、拡張機能をDotfiles形式(GitHub)で管理し、環境を共有する。",
+          link: "https://note.mu/teitei_tk/n/n7204cb8d97c5"
+        }
+      ]
+    }
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-typescript"]
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,13 @@ interface IIndexPageProps {
       siteMetadata: {
         name: string;
         tagline: string;
+        note: {
+          url: string;
+          posts: Array<{
+            title: string;
+            link: string;
+          }>;
+        };
       };
     };
   };
@@ -20,18 +27,36 @@ export const indexPageQuery = graphql`
       siteMetadata {
         name
         tagline
+        note {
+          posts {
+            title
+            link
+          }
+        }
       }
     }
   }
 `;
 
+export const NotePostComponent = (props: { title: string; link: string }) => {
+  return (
+    <div>
+      <h5>{props.title}</h5>
+      <p>{props.link}</p>
+    </div>
+  );
+};
+
 const Component: React.SFC<IIndexPageProps> = props => {
-  const { name, tagline } = props.data.site.siteMetadata;
+  const { name, tagline, note } = props.data.site.siteMetadata;
 
   return (
     <div className={styles.container}>
       <h1>{name}</h1>
       <p>{tagline}</p>
+      {note.posts.map(prop => {
+        return <NotePostComponent {...prop} />;
+      })}
     </div>
   );
 };
